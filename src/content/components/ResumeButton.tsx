b72debeaed2c873c786cc2cabec58c4c545245ce
Alex <alex@devarno.com>
2023-03-28 08:33:06 +0100

    CV button link

--- src/content/components/ResumeButton.tsx
+++ src/content/components/ResumeButton.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/system";
 
 export const ResumeButton = () => (
     <Box justifyContent="center" sx={{ width: "100%", mt: 4 }} display="flex" alignItems="center">
-        <a className="button" href="/pdf/Alessandro Arno Resumé.pdf" download>
+        <a className="button" href="/pdf/Alessandro Arnò Resumé.pdf" download>
             <span className="button-text">Download CV</span>
             <span className="button-icon fa fa-download"></span>
         </a>
