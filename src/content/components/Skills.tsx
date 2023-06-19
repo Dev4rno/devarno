f5c1a7745ee1fe192c36bfb3548d2c6687bb8f4a
Alex <alex@devarno.com>
2023-06-19 15:24:26 +0100

    Skills

--- src/content/components/Skills.tsx
+++ src/content/components/Skills.tsx
@@ -5,7 +5,7 @@ enum SkillTypes {
     framework = "Framework",
     library = "Library",
     operation = "DevOps",
-    cad = "Computer Aided Design",
+    app = "Application",
 }
 
 interface Skill {
@@ -16,6 +16,44 @@ interface Skill {
 }
 
 const skills: Skill[] = [
+    // Frameworks
+    {
+        type: SkillTypes.framework,
+        name: "React",
+        pct: 80,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+    },
+    {
+        type: SkillTypes.framework,
+        name: "Next.js",
+        pct: 80,
+        img: "https://www.rlogical.com/wp-content/uploads/2021/08/Rlogical-Blog-Images-thumbnail.png",
+    },
+    {
+        type: SkillTypes.framework,
+        name: "NestJS",
+        pct: 60,
+        img: "https://static-00.iconduck.com/assets.00/nestjs-icon-512x510-9nvpcyc3.png",
+    },
+    {
+        type: SkillTypes.framework,
+        name: "NodeJS / Express",
+        pct: 75,
+        img: "https://seeklogo.com/images/N/node-js-logo-F4F55CD2D0-seeklogo.com.png",
+    },
+    {
+        type: SkillTypes.framework,
+        name: "FastAPI",
+        pct: 45,
+        img: "https://cdn.worldvectorlogo.com/logos/fastapi.svg",
+    },
+    {
+        type: SkillTypes.framework,
+        name: "Gin",
+        pct: 45,
+        img: "https://avatars.githubusercontent.com/u/15729372?s=280&v=4",
+    },
+
     {
         type: SkillTypes.language,
         name: "Python",
@@ -42,11 +80,10 @@ const skills: Skill[] = [
     },
     {
         type: SkillTypes.language,
-        name: "MATLAB",
-        pct: 40,
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Matlab_Logo.png/667px-Matlab_Logo.png",
+        name: "Go",
+        pct: 35,
+        img: "https://support.discord.com/hc/user_images/w3-GkZneLZmfg6odFGLqoQ.png",
     },
-
     {
         type: SkillTypes.language,
         name: "Bash",
@@ -61,18 +98,6 @@ const skills: Skill[] = [
     },
 
     {
-        type: SkillTypes.framework,
-        name: "ReactJS",
-        pct: 80,
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-    },
-    {
-        type: SkillTypes.framework,
-        name: "NextJS",
-        pct: 80,
-        img: "https://www.rlogical.com/wp-content/uploads/2021/08/Rlogical-Blog-Images-thumbnail.png",
-    },
-    {
         type: SkillTypes.operation,
         name: "MongoDB",
         pct: 40,
@@ -80,13 +105,9 @@ const skills: Skill[] = [
     },
     {
         type: SkillTypes.operation,
-        name: (
-            <>
-                PostgreSQL <span style={{ color: "#ffb400" }}>/</span> GraphQL
-            </>
-        ),
+        name: "GraphQL",
         pct: 20,
-        img: "https://cdn.iconscout.com/icon/free/png-256/postgresql-3521647-2945091.png",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/2048px-GraphQL_Logo.svg.png",
     },
     {
         type: SkillTypes.operation,
@@ -120,74 +141,116 @@ const skills: Skill[] = [
     },
     {
         type: SkillTypes.library,
-        name: (
-            <>
-                pandas <span style={{ color: "#ffb400" }}>/</span> R Studio
-            </>
-        ),
-        pct: 50,
-        img: "https://gitlab.com/uploads/-/system/project/avatar/18103103/pngguru.com.png",
+        name: "Matplotlib",
+        pct: 65,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Matplotlib_icon.svg/1200px-Matplotlib_icon.svg.png",
     },
     {
         type: SkillTypes.library,
+        name: "Bokeh",
+        pct: 35,
+        img: "https://numfocus.org/wp-content/uploads/2017/11/bokeh-logo-300.png",
+    },
+    {
+        type: SkillTypes.library,
+        name: "Pandas",
+        pct: 55,
+        img: "https://www.cilans.net/wp-content/uploads/2019/12/Python-Pandas-logo.png",
+    },
+    {
+        type: SkillTypes.app,
+        name: "Chrome DevTools",
+        pct: 65,
+        img: "https://static-00.iconduck.com/assets.00/chrome-devtools-icon-512x512-8iaxdppx.png",
+    },
+    {
+        type: SkillTypes.app,
+        name: "WireShark",
+        pct: 42,
+        img: "https://cdn.icon-icons.com/icons2/1495/PNG/512/wireshark_103123.png",
+    },
+    {
+        type: SkillTypes.app,
+        name: "MATLAB",
+        pct: 40,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Matlab_Logo.png/667px-Matlab_Logo.png",
+    },
+    {
+        type: SkillTypes.app,
         name: "DOORS",
         pct: 60,
         img: "https://softacus.com/images/2022/02/04/ibm-engineering-requirements-management-doors-next.png",
     },
-
     {
-        type: SkillTypes.cad,
+        type: SkillTypes.app,
         name: "SolidWorks",
         pct: 50,
         img: "https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/451b0dc471cd4cdc764c2a586f4e91b0/medium.png",
     },
     {
-        type: SkillTypes.cad,
+        type: SkillTypes.app,
         name: "COMSOL",
-        pct: 30,
+        pct: 40,
         img: "https://www.file-extensions.org/imgs/app-icon/128/5933/comsol-multiphysics-icon.png",
+    },
+    {
+        type: SkillTypes.app,
+        name: "XFLR5",
+        pct: 60,
+        img: "https://s.softdeluxe.com/icons/png/128/2931/2931107.png",
+    },
+    {
+        type: SkillTypes.app,
+        name: "ANSYS",
+        pct: 30,
+        img: "https://pbs.twimg.com/profile_images/1410748825824894978/RVKA6tlT_400x400.jpg",
     },
 ];
 
 export const Skills: FunctionComponent = (props): JSX.Element => (
     <>
         <h3 className="text-uppercase pb-4 pb-sm-5 mb-3 mb-sm-0 text-center custom-title ft-wt-600">Skills</h3>
-        {skills.map(({ type, name, pct, img }, i) => (
-            <div className="col-6 col-md-3 mb-3 mb-sm-5" key={i}>
-                <div className={`c100 p${pct}`}>
-                    <div className="slice">
-                        <div className="bar"></div>
-                        <div className="fill"></div>
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            height: "100%",
-                        }}
-                    >
-                        <img
-                            src={img}
-                            alt={`Skill${i}`}
+        {skills
+            .sort((a, b) => b.pct - a.pct)
+            .map(({ type, name, pct, img }, i) => (
+                <div className="col-6 col-md-3 mb-3 mb-sm-5" key={i}>
+                    <div className={`c100 p${pct}`}>
+                        <div className="slice">
+                            <div className="bar"></div>
+                            <div className="fill"></div>
+                        </div>
+                        <div
                             style={{
-                                height: 60,
-                                // top: 37,
-                                // left: 32,
-                                width: "auto",
-                                // position: "absolute",
-                                zIndex: 9999999999999,
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                height: "100%",
                             }}
-                        />
+                        >
+                            <img
+                                src={img}
+                                alt={`Skill${i}`}
+                                style={{
+                                    height: 60,
+                                    // top: 37,
+                                    // left: 32,
+                                    width: "auto",
+                                    // position: "absolute",
+                                    zIndex: 9999999999999,
+                                }}
+                            />
+                        </div>
                     </div>
+                    <h6 className="open-sans-font text-center mt-2 mt-sm-4" style={{ letterSpacing: 1 }}>
+                        {name}
+                    </h6>
+                    <p
+                        className="open-sans-font text-center"
+                        style={{ fontSize: 12, color: "#ffb400", letterSpacing: 1 }}
+                    >
+                        <i>{type}</i>
+                    </p>
                 </div>
-                <h6 className="open-sans-font text-center mt-2 mt-sm-4" style={{ letterSpacing: 1 }}>
-                    {name}
-                </h6>
-                <p className="open-sans-font text-center" style={{ fontSize: 12, color: "#ffb400", letterSpacing: 1 }}>
-                    <i>{type}</i>
-                </p>
-            </div>
-        ))}
+            ))}
     </>
 );
