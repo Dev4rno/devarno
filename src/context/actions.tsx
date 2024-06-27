a96d1c2a30b8e13a9b38fcb3da54d1b1ec8421bf
Alex <alex@devarno.com>
2024-06-27 09:14:22 +0200

    Try again

--- src/context/actions.tsx
+++ src/context/actions.tsx
@@ -1,15 +1,5 @@
 import api from "../lib/axios";
 
-// export const sendEmail = async (formData: any): Promise<any> => {
-//     try {
-//         const res = await api.post("/mail", JSON.stringify(formData));
-//         return res.data;
-//     } catch (error) {
-//         console.error("Error sending email:", error);
-//         throw error;
-//     }
-// };
-
 export const sendEmail = async (formData: any): Promise<any> => {
     try {
         const res = await api.post("/mail", formData);
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,27 +1,6 @@
-// import axios from "axios";
-
-// // Base URL
-// type NodeEnvironment = "development" | "production" | "test";
-// const NODE_ENV: NodeEnvironment = process.env.NODE_ENV;
-// const DEV_URL = process.env.API_URL_DEVELOPMENT || "http://localhost:3000/api";
-// const PROD_URL = process.env.API_URL_PRODUCTION || "https://devarno.com/api";
-// export const baseURL = NODE_ENV == "production" ? PROD_URL : DEV_URL;
-
-// // Headers
-// const headers = {
-//     "Content-Type": "application/json",
-// };
-
-// // Axios instance
-// const api = axios.create({
-//     baseURL,
-//     headers,
-// });
-
-// export default api;
 import axios from "axios";
 
-const baseURL = "https://www.devarno.com/api"; // Use the same base URL
+const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "https://www.devarno.com/api";
 
 const api = axios.create({
     baseURL,
